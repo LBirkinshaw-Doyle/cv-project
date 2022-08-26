a6f4e4445191eed4be72dbaef9786a754b6bdcc2
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-08-26 14:13:17 +0100

    add GeneralInfoDisplay component to display general info and edit button to allow edits

--- src/components/GeneralInfoDisplay.js
+++ src/components/GeneralInfoDisplay.js
@@ -4,9 +4,21 @@ class GeneralInfoDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+  handleEdit(event) {
+    event.preventDefault();
+    this.props.toggleEdit(event);
   }
   render() {
-    return <div></div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1 id="general-info-name">{this.props.name}</h1>
+        <span id="general-info-email">{this.props.email}</span>
+        <span id="general-info-phone">{this.props.phoneNumber}</span>
+        <button onClick={this.handleEdit}>Edit</button>
+      </form>
+    );
   }
 }
 
