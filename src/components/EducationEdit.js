556b39daec037a7618543d7dfd69b3272cd3831e
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-09-01 11:08:15 +0100

    add single item edit to education

--- src/components/EducationEdit.js
+++ src/components/EducationEdit.js
@@ -1,68 +1,49 @@
 import React from 'react';
 
-//import './GeneralInfoEdit.css';
+import EducationEditInput from './EducationEditInput';
+
+//import './EducationEdit.css';
 
 class EducationEdit extends React.Component {
   constructor(props) {
     super(props);
-    /*this.state = {
-      nameValue: this.props.name,
-      emailValue: this.props.email,
-      phoneNumberValue: this.props.phoneNumber,
+    this.state = {
+      education: [],
     };
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);*/
+    this.handleEducationItemChange = this.handleEducationItemChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  /*
-  handleNameChange(event) {
-    this.setState({ nameValue: event.target.value });
-  }
-  handleEmailChange(event) {
-    this.setState({ emailValue: event.target.value });
-  }
-  handlePhoneNumberChange(event) {
-    this.setState({ phoneNumberValue: event.target.value });
+
+  handleEducationItemChange(index, school, title, date) {
+    let educationArray = this.state.education.slice();
+    educationArray[index] = { school: school, title: title, date: date };
+    this.setState({ education: educationArray });
   }
   handleSubmit(event) {
     event.preventDefault();
-    this.props.updateName(this.state.nameValue);
-    this.props.updateEmail(this.state.emailValue);
-    this.props.updatePhoneNumber(this.state.phoneNumberValue);
+    if (this.state.education[0] !== undefined)
+      this.props.updateEducation(this.state.education);
     this.props.toggleEdit(event);
-  }*/
+  }
   render() {
+    let display = [];
+    this.props.educationList.forEach((educationItem, index) => {
+      display.push(
+        <EducationEditInput
+          school={educationItem.school}
+          title={educationItem.title}
+          date={educationItem.date}
+          index={index}
+          key={educationItem.school + educationItem.title}
+          handleChange={this.handleEducationItemChange}
+        />
+      );
+    });
     return (
-      /*
-      <form id="general-info-form" onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.nameValue}
-            onChange={this.handleNameChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={this.state.emailValue}
-            onChange={this.handleEmailChange}
-          />
-        </label>
-        <label>
-          Phone Number:
-          <input
-            type="tel"
-            value={this.state.phoneNumberValue}
-            onChange={this.handlePhoneNumberChange}
-          />
-        </label>
+      <form id="education-form" onSubmit={this.handleSubmit}>
+        {display}
         <input type="submit" value="Submit" />
-      </form>*/
-      <div></div>
+      </form>
     );
   }
 }
---
+++ src/components/EducationEditInput.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+//import './EducationEditInput.css';
+
+class EducationEditInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      schoolValue: this.props.school,
+      titleValue: this.props.title,
+      dateValue: this.props.date,
+    };
+    this.handleSchoolChange = this.handleSchoolChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state !== prevState) {
+      this.props.handleChange(
+        this.props.index,
+        this.state.schoolValue,
+        this.state.titleValue,
+        this.state.dateValue
+      );
+    }
+  }
+  handleSchoolChange(event) {
+    this.setState({ schoolValue: event.target.value });
+  }
+  handleTitleChange(event) {
+    this.setState({ titleValue: event.target.value });
+  }
+  handleDateChange(event) {
+    this.setState({ dateValue: event.target.value });
+  }
+  render() {
+    return (
+      <div id="education-item">
+        <label>
+          School:
+          <input
+            type="text"
+            value={this.state.schoolValue}
+            onChange={this.handleSchoolChange}
+          />
+        </label>
+        <label>
+          Course:
+          <input
+            type="text"
+            value={this.state.titleValue}
+            onChange={this.handleTitleChange}
+          />
+        </label>
+        <label>
+          date:
+          <input
+            type="date"
+            value={this.state.dateValue}
+            onChange={this.handleDateChange}
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default EducationEditInput;
